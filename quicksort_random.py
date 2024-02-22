23e0fd4ca140e4de5201af788d8f63a96f1c8e83
MD OMER AHMED <105853902+OMER7042@users.noreply.github.com>
2024-02-22 00:39:08 -0600

    Update quicksort_random.py

--- quicksort_random.py
+++ quicksort_random.py
@@ -6,10 +6,10 @@ def random_pivot_quicksort(array):
     if len(array) <= 1:
         return array
     pivot = randpiv.choice(array)
-    lesser = [x for x in array if x < pivot]
+    smaller = [x for x in array if x < pivot]
     equals = [x for x in array if x == pivot]
     bigger = [x for x in array if x > pivot]
-    return random_pivot_quicksort(lesser) + equals + random_pivot_quicksort(bigger)
+    return random_pivot_quicksort(smaller) + equals + random_pivot_quicksort(bigger)
 
 # I demonstrated same example to understand the working of two different logics
 array = [10, 13, 15, 17, 8, 9, 1]
